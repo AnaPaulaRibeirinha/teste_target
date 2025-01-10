225aa6f33704bead3648b2b58d41c586ee70da04
ana paula melo <anahoran.tommo@gmail.com>
2025-01-10 09:35:14 -0300

    limpeza e ajuste no código

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -22,15 +22,7 @@ public class App
         System.out.println( "QUESTÃO 2:" );
 
         try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Informe um número para verificar se pertence à sequência de Fibonacci:");
-            int number = scanner.nextInt();
-
-            if(appService.isFibonacci(number)) {
-                System.out.println("O número " + number + " pertence à sequência de Fibonacci.");
-            } else {
-                System.out.println("O número " + number + " NÃO pertence à sequência de Fibonacci.");
-            }
+            appService.fibonacciResolution();
         } catch (Exception e) {
             throw new RuntimeException("Houve algum problema ao ler o número informado");
         }
@@ -40,36 +32,9 @@ public class App
         System.out.println( "QUESTÃO 3:" );
 
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootNode = objectMapper.readTree(new File("dados.json"));
-
-            List<Double> faturamentos = new ArrayList<>();
-
-            for (JsonNode node : rootNode) {
-                double valor = node.get("valor").asDouble();
-                if (valor > 0) { // Ignorar dias sem faturamento
-                    faturamentos.add(valor);
-                }
-            }
-
-            if (faturamentos.isEmpty()) {
-                System.out.println("Nenhum dia de faturamento registrado.");
-                return;
-            }
-
-            double menorFaturamento = faturamentos.stream().min(Double::compare).orElse(0.0);
-            double maiorFaturamento = faturamentos.stream().max(Double::compare).orElse(0.0);
-
-            double soma = faturamentos.stream().mapToDouble(Double::doubleValue).sum();
-            double media = soma / faturamentos.size();
-
-            long diasAcimaDaMedia = faturamentos.stream().filter(f -> f > media).count();
-
-            System.out.printf("Menor valor de faturamento: %.2f\n", menorFaturamento);
-            System.out.printf("Maior valor de faturamento: %.2f\n", maiorFaturamento);
-            System.out.printf("Número de dias com faturamento acima da média: %d\n", diasAcimaDaMedia);
-        } catch (IOException e) {
-            System.err.println("Erro ao ler o arquivo JSON: " + e.getMessage());
+            appService.dataBilling();
+        } catch (Exception e) {
+            throw new RuntimeException("Houve falha no processo de dados do faturamento diario");
         }
 
         System.out.println( "/////////////////////////////////////////////////////////////" );
@@ -82,15 +47,7 @@ public class App
 
         System.out.println( "QUESTÃO 5:" );
 
-        try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Informe uma frase");
-            String frase = scanner.nextLine();
-            System.out.println("Frase informada: " + frase);
-            System.out.println("Frase invertidade: " + appService.reverseString(frase));
-        } catch (Exception e) {
-            throw new RuntimeException("Houve algum problema ao ler a frase");
-        }
+        appService.reverseStringProcess();
 
     }
 }
--- src/main/java/org/example/AppService.java
+++ src/main/java/org/example/AppService.java
@@ -1,23 +1,15 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AppService {
-
-    public boolean isFibonacci(int number) {
-        if (number < 0) {
-            return false;
-        }
-
-        int a = 0, b = 1;
-        while (a <= number) {
-            if (a == number) {
-                return true;
-            }
-            int next = a + b;
-            a = b;
-            b = next;
-        }
-        return false;
-    }
 
     public void monthlyBilling() {
         double sp = 67836.43;
@@ -35,8 +27,85 @@ public class AppService {
         System.out.printf("Outros: %.2f%%%n", (outros / total) * 100);
     }
 
-    public String reverseString(String str) {
+    public void dataBilling() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(new File("dados.json"));
+
+            List<Double> faturamentos = new ArrayList<>();
+
+            for (JsonNode node : rootNode) {
+                double valor = node.get("valor").asDouble();
+                if (valor > 0) { // Ignorar dias sem faturamento
+                    faturamentos.add(valor);
+                }
+            }
+
+            if (faturamentos.isEmpty()) {
+                System.out.println("Nenhum dia de faturamento registrado.");
+                return;
+            }
+
+            double menorFaturamento = faturamentos.stream().min(Double::compare).orElse(0.0);
+            double maiorFaturamento = faturamentos.stream().max(Double::compare).orElse(0.0);
+
+            double soma = faturamentos.stream().mapToDouble(Double::doubleValue).sum();
+            double media = soma / faturamentos.size();
+
+            long diasAcimaDaMedia = faturamentos.stream().filter(f -> f > media).count();
+
+            System.out.printf("Menor valor de faturamento: %.2f\n", menorFaturamento);
+            System.out.printf("Maior valor de faturamento: %.2f\n", maiorFaturamento);
+            System.out.printf("Número de dias com faturamento acima da média: %d\n", diasAcimaDaMedia);
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo JSON: " + e.getMessage());
+        }
+    }
+
+    public void reverseStringProcess() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Informe uma frase");
+            String frase = scanner.nextLine();
+            System.out.println("Frase informada: " + frase);
+            System.out.println("Frase invertidade: " + reverseString(frase));
+        } catch (Exception e) {
+            throw new RuntimeException("Houve algum problema ao ler a frase");
+        }
+    }
+
+    public void fibonacciResolution() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe um número para verificar se pertence à sequência de Fibonacci:");
+        int number = scanner.nextInt();
+
+        if(isFibonacci(number)) {
+            System.out.println("O número " + number + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println("O número " + number + " NÃO pertence à sequência de Fibonacci.");
+        }
+    }
+
+    private String reverseString(String str) {
         StringBuilder sb = new StringBuilder(str);
         return sb.reverse().toString(); // Reverso da string
     }
+
+    private boolean isFibonacci(int number) {
+        if (number < 0) {
+            return false;
+        }
+
+        int a = 0, b = 1;
+        while (a <= number) {
+            if (a == number) {
+                return true;
+            }
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        return false;
+    }
+
 }
